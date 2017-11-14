05a76811074f70903aecccfbb870f9946e5121fd
xslet <xslet@yahoo.co.jp>
2017-11-15 08:23:23 +0900

    Remove DedupePlugin from .webpack.config.js

--- .webpack.config.js
+++ .webpack.config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var webpack = require('webpack');
+const webpack = require('webpack');
 
 module.exports = {
   output: {
@@ -10,7 +10,6 @@ module.exports = {
   },
   devtool: 'source-map',
   plugins: [
-    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
       output: { comments: false },
